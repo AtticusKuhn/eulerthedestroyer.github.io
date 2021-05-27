eac495f422a269be26f012e804cb56e7b2b7667a
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-05-26 18:14:10 -0700

    Initial dist subtree commit

---
+++ _next/static/ye7VuJKsr5tM2FnHY8lRg/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fmy-projects","\u002Fmy-projects\u002Fhtml\u002F[id]","\u002Fmy-projects\u002Fhtml\u002F[id]\u002F[...project]","\u002Fblog\u002F[article]","\u002Fmy-projects\u002Fjs\u002F[js]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
